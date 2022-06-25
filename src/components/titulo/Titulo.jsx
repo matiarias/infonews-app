88207779917640608957c5509e00cd0352b33b24
Matias Arias <matt.arias182@gmail.com>
2022-06-25 01:20:33 -0300

    ase añadio 2 google fonts para el navbar y body, y fnavbar finalizado

--- src/components/titulo/Titulo.jsx
+++ src/components/titulo/Titulo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../titulo/titulo.css";
+import logo from "../../assets/news-logo.png";
 
 const Titulo = () => {
   return (
     <div>
-      <nav class="navbar bg-success">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
+      <nav className="navbar">
+        <div className="container d-flex justify-content-center align-items-center ">
+          <img className="news-logo" src={logo} alt="logo" />
+          <h2 className="title-nav">Infonews</h2>
         </div>
       </nav>
     </div>
